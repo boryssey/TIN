8555dc1a94dd58f3831a10f6a652d9aef1a98198
boryssey <bor920@gmail.com>
2018-11-05 00:00:11 +0100

    changed sorting in 4h

--- 4h.js
+++ 4h.js
@@ -1,5 +1,5 @@
-function secondLowestGreatest(arr){
-  arr = arr.sort();
-  return [arr[1], arr[arr.length-2]];
+function secondLowestGreatest(arr) {
+  arr = arr.sort((a, b) => a - b);
+  return [arr[1], arr[arr.length - 2]];
 }
-console.log("second lowest and second greatest of [3,1,2,5,8] = " + secondLowestGreatest([3,1,2,5,8]).toString());
+console.log("second lowest and second greatest of [3,1,2,5,8] = " + secondLowestGreatest([3, 1, 2, 5, 8]).toString());
